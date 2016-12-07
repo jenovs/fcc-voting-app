1dc3bc76272d94db590f3618f5f257f584f94188
Viktors Jenovs <jviktors@gmail.com>
2016-12-07 17:50:18 +0200

    Allow Access-Control

--- server/server.js
+++ server/server.js
@@ -13,6 +13,19 @@ app.use(bodyParser.json());
 
 app.disable('x-powered-by');
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
 // app.get('/', (req, res) => {
 // });
 
